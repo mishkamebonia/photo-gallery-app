08550c92d713ceb17a8da4ccde3cd93390b3ef33
mishkamebonia <mishkas.mebonia@gmail.com>
2024-03-02 08:36:08 +0400

    refactor

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,51 +14,71 @@ interface Image {
 const HomePage = () => {
   const [images, setImages] = useState<Image[]>([]);
   const [searchValue, setSearchValue] = useState<string>("");
+  const [typingTimeout, setTypingTimeout] = useState<number | undefined>(
+    undefined
+  );
 
   const perPage = 20;
   const order = "popular";
+  const typingDelay = 1000;
 
-  const fetchImages = async () => {
+  const fetchImages = async (query: string) => {
     try {
-      const response = await fetch(
-        `${url}/search/photos?page=1&client_id=${accessKey}&order_by=${order}&per_page=${perPage}&query=${searchValue}`
-      );
+      let apiUrl = `${url}/photos?page=1&client_id=${accessKey}&order_by=${order}&per_page=${perPage}`;
+
+      if (query) {
+        apiUrl = `${url}/search/photos?page=1&client_id=${accessKey}&order_by=${order}&per_page=${perPage}&query=${query}`;
+      }
+
+      const response = await fetch(apiUrl);
       const data = await response.json();
-      const imagesData = data.results.map((result: any) => ({
-        id: result.id,
-        urls: {
-          small: result.urls.small,
-        },
-        description: result.alt_description || "",
-      }));
+      const imagesData = data.results
+        ? data.results.map((result: any) => ({
+            id: result.id,
+            urls: {
+              small: result.urls.small,
+            },
+            description: result.alt_description || "",
+          }))
+        : [];
+
       setImages(imagesData);
     } catch (err) {
       console.error("Error ", err);
     }
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSearch = (value: string) => {
+    setSearchValue(value);
 
-    fetchImages();
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+
+    const timeoutId = setTimeout(() => {
+      fetchImages(value);
+    }, typingDelay);
+    setTypingTimeout(timeoutId);
   };
 
   useEffect(() => {
-    fetchImages();
-  }, [searchValue]);
+    return () => {
+      if (typingTimeout) {
+        clearTimeout(typingTimeout);
+      }
+    };
+  }, [typingTimeout, searchValue]);
 
   return (
     <div>
       <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="search"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            className="search"
-            placeholder="Search image"
-          />
-        </form>
+        <input
+          type="search"
+          value={searchValue}
+          onChange={(e) => handleSearch(e.target.value)}
+          className="search"
+          placeholder="Search image"
+        />
       </div>
       <div className="images-grid">
         {images.map((image) => (
